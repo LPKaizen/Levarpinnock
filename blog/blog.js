4a28d219d2260fd1665c67d0a31c8f7ca8029d47
LPKaizen <Levar.pinnock@gmail.com>
2024-07-28 18:10:51 -0400

    Seperated Blog posts for more efficiency

--- blog/blog.js
+++ blog/blog.js
@@ -1,37 +1,3 @@
-//show and hide div elements on click
-function Blogpost1() {
-    document.getElementById("post1").style.visibility = "visible";
-    document.getElementById("post1").style.display = "block";
-
-    document.getElementById("post2").style.visibility = "hidden";
-    document.getElementById("post2").style.display = "none";
-
-    document.getElementById("post3").style.visibility = "hidden";
-    document.getElementById("post3").style.display = "none";
-}
-
-function Blogpost2() {
-    document.getElementById("post2").style.visibility = "visible";
-    document.getElementById("post2").style.display = "block";
-
-    document.getElementById("post1").style.visibility = "hidden";
-    document.getElementById("post1").style.display = "none";
-
-    document.getElementById("post3").style.visibility = "hidden";
-    document.getElementById("post3").style.display = "none";
-}
-
-function Blogpost3() {
-    document.getElementById("post3").style.visibility = "visible";
-    document.getElementById("post3").style.display = "block";
-
-    document.getElementById("post2").style.visibility = "hidden";
-    document.getElementById("post2").style.display = "none";
-
-    document.getElementById("post1").style.visibility = "hidden";
-    document.getElementById("post1").style.display = "none";
-}
-
 
 function menuToggle() {
     if(document.getElementById("slides").style.left == "-520vw")
